19396585627b04ca1d28d666b5bf129e36473d4a
Ahmed Abd El-Mawla <37018787+Ahmed-Abd-Elmawla@users.noreply.github.com>
2025-06-24 15:17:08 +0300

    Create redirect rules - 2025-06-24 12:17:07

---
+++ redirect-rules.js
@@ -0,0 +1,15 @@
+const RedirectRules = {
+    "/zh/tour/Coptic-Cairo-Adventure-History-Culture-and%20Family%20Fun": {
+        "redirect": {
+            "to": "/zh/tour/coptic-cairo-adventure-history-culture-and-family-fun",
+            "statusCode": 301
+        }
+    },
+    "/zh/tour/Coptic-Cairo-Adventure-History-Culture-and%20Family%20Fun0": {
+        "redirect": {
+            "to": "/zh/tour/Coptic-Cairo-Adventure-History-Culture",
+            "statusCode": 301
+        }
+    }
+}
+export default RedirectRules;
